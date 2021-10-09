587203cb79071f44fdcfeb3060598d5b131a3402
DerVerruckte <michael.heinhold@gmail.com>
2021-10-09 09:32:23 -0600

    added functions to add to database and console.table

--- index.js
+++ index.js
@@ -11,10 +11,9 @@ function viewDepartments() {
             console.log(err.message);
             return;
         }
-        console.log(rows);
+        console.table(rows);
     });
 }
-viewDepartments();
 
 //views all the roles
 function viewRoles() {
@@ -25,10 +24,9 @@ function viewRoles() {
             console.log(err.message);
             return;
         }
-        console.log(rows);
+        console.table(rows);
     });
 }
-viewRoles();
 
 function viewEmployees () {
     const mysql = `SELECT * FROM employees`;
@@ -38,10 +36,52 @@ function viewEmployees () {
             console.log(err.message);
             return;
         }
+        console.table(rows);
+    });
+}
+
+function addNewDepartment(name) {
+    const mysql = `INSERT INTO departments (name)
+        VALUES (?)`
+    
+        db.query(mysql, name, (err, rows) => {
+        if(err) {
+            console.log(err.message);
+            return;
+        }
         console.log(rows);
     });
 }
-viewEmployees();
+
+function addNewRole(title, salary, departmentId){
+    const mysql = `INSERT INTO roles (title, salary, departments_id)
+        VALUES (?, ?, ?)`
+
+    const params = [title, salary, departmentId];
+
+        db.query(mysql, params, (err, rows) => {
+            if(err) {
+                console.log(err.message);
+                return;
+            }
+            console.log(rows);
+        });
+}
+
+function addNewEmployee(firstName, lastName, rolesId, managerId){
+    const mysql = `INSERT INTO employees (first_name, last_name, roles_id, manager_id)
+        VALUES (?, ?, ?, ?)`
+    
+    const params = [firstName, lastName, rolesId, managerId];
+
+    db.query(mysql, params, (err, rows) => {
+        if (err) {
+            console.log(err.message);
+            return;
+        }
+        console.log (rows);
+    });
+}
 
 //runs the prompt
 // const questionuser = function(){ 
