eacfce4bd448c0abd2623422ac99a43121bd6f8e
DerVerruckte <michael.heinhold@gmail.com>
2021-10-12 15:39:45 -0600

    choices refreshes

--- src/prompt.js
+++ src/prompt.js
@@ -1,19 +1,28 @@
 const db = require('../db/connection');
 
+//arrays for options
+var departmentsArr =[];
+var roleArr = [];
+var managersArr = ['Is a manager'];
+var employeeArr = [];
+
+/*The following functions allow the choices to be options already in the database
+After the functions there is a setInterval that calls the function again every 3 seconds, 
+this is to make sure the choices are up to date. */
+
 //options for departments
-departmentsArr =[];
 function selectDepartment() {
-    db.query("SELECT * FROM departments", function(err, res){
-        if(err) throw err;
-        for(var i = 0; i< res.length; i++) {
-            departmentsArr.push(res[i].name);
-        }
-    })
-    return departmentsArr;
+        db.query("SELECT * FROM departments", function(err, res){
+            if(err) throw err;
+            for(var i = 0; i< res.length; i++) {
+                departmentsArr.push(res[i].name);
+            }
+        })
+        return departmentsArr;
 }
+setInterval(selectDepartment, (1000)*3);
 
 //options for roles
-var roleArr = [];
 function selectRole() {
     db.query("SELECT * FROM roles", function(err, res) {
         if (err) throw err
@@ -23,9 +32,9 @@ function selectRole() {
     })
     return roleArr;
 }
+setInterval(selectRole, (1000)*3);
 
 //options for managers
-var managersArr = ['Is a manager'];
 function selectManager() {
     db.query("SELECT first_name, last_name FROM employees WHERE manager_id IS NULL", function(err, res) {
         if (err) throw err
@@ -35,9 +44,9 @@ function selectManager() {
     })
     return managersArr;
 }
+setInterval(selectManager, (1000)*3);
 
 //options for employee
-var employeeArr = [];
 function selectEmployee() {
     db.query("SELECT first_name, last_name FROM employees", function(err, res) {
         if (err) throw err
@@ -47,6 +56,7 @@ function selectEmployee() {
     })
     return employeeArr;
 }
+setInterval(selectEmployee, (1000)*3);
 
 const prompt = [
     {
@@ -77,7 +87,7 @@ const prompt = [
         message: 'What is the salary for this role?'
     },
     {
-        type: 'rawlist',
+        type: 'list',
         when: ({ option }) => option === 'Add a role',
         name: 'departmentId',
         message: 'What department is this role under?',
@@ -96,28 +106,28 @@ const prompt = [
         message: `What is the employee's last name?`
     },
     {
-        type: 'rawlist',
+        type: 'list',
         when: ({ option }) => option === 'Add an employee',
         name: 'roleId',
         message: `What is the employee's role?`,
         choices: selectRole()
     },
     {
-        type: 'rawlist',
+        type: 'list',
         when: ({ option }) => option === 'Add an employee',
         name: 'managerId',
         message: `What manager does this employee work under?`,
         choices: selectManager()
     },
     {
-        type: 'rawlist',
+        type: 'list',
         when: ({ option }) => option === 'Update an employee role',
         name: 'employeeId',
         message: 'For which employee would you like to update their role?',
         choices: selectEmployee()
     },
     {
-        type: 'rawlist',
+        type: 'list',
         when: ({ option }) => option === 'Update an employee role',
         name: 'newRoleId',
         message: 'What is their new role?',
